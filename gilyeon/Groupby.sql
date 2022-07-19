a33ee7b663d016805baed952726ee1ee0e82d513
gilyeon <kil2852@naver.com>
2022-07-19 22:01:31 +0900

    7/19 알고리즘

--- gilyeon/Groupby.sql
+++ gilyeon/Groupby.sql
@@ -1,4 +1,4 @@
--- 고양이와 개는 몇 마리 있을까
+﻿-- 고양이와 개는 몇 마리 있을까
 SELECT ANIMAL_TYPE, count(ANIMAL_TYPE) AS count 
 from ANIMAL_INS
 group by ANIMAL_TYPE
@@ -10,5 +10,20 @@ from ANIMAL_INS
 group by NAME
 having count(NAME) >= 2
 order by NAME asc;
+
+-- 입양 시각 구하기(1)
+SELECT HOUR(DATETIME) HOUR, COUNT(DATETIME) COUNT
+FROM ANIMAL_OUTS
+GROUP BY HOUR(DATETIME)
+HAVING HOUR BETWEEN 9 AND 19
+ORDER BY HOUR ASC;
+
+-- 입양 시각 구하기(2)
+SET @hour := -1; -- 변수 선언
+
+SELECT (@hour := @hour + 1) as HOUR, 
+(SELECT COUNT(DATETIME) FROM ANIMAL_OUTS WHERE HOUR(DATETIME) = @hour) as COUNT
+FROM ANIMAL_OUTS
+WHERE @hour < 23;
 
 
