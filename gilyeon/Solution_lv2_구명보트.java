0ec6337d60cfc65ea5d199db4681288b5e2fec63
gilyeon <kil2852@naver.com>
2022-10-13 22:53:26 +0900

    10/13 알고리즘

---
+++ gilyeon/Solution_lv2_구명보트.java
@@ -0,0 +1,19 @@
+﻿import java.util.Arrays;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int index = 0;
+
+        for (int i = people.length - 1; i >= index; i--) { // 가장 큰 수부터 시작
+            if (people[i] + people[index] <= limit) {
+                index++; // idx 2개 건너뛰기
+                answer++;
+            }
+            else {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
