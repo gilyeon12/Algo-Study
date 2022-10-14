091fe6fdea26b83403e2f9d04a611657195d74c2
gilyeon <kil2852@naver.com>
2022-10-14 21:19:01 +0900

    10/14 알고리즘

---
+++ gilyeon/Solution_lv2_예상대진표.java
@@ -0,0 +1,29 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+        while(a != b){
+
+            a = (a % 2 == 0) ? a / 2 : a / 2 + 1;
+            b = (b % 2 == 0) ? b / 2 : b / 2 + 1;
+
+
+            // if(a % 2 == 0){
+            //     a = a / 2;
+            // }
+            // else{
+            //     a = a / 2 + 1;
+            // }
+            // if(b % 2 == 0){
+            //     b = b / 2;
+            // }
+            // else{
+            //     b = b / 2 + 1;
+            // }
+            answer++;
+        }
+
+        return answer;
+    }
+}
