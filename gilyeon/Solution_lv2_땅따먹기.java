150b6bccc43cf4ea2aa15660f57a4f732af8f3ef
gilyeon <kil2852@naver.com>
2022-10-28 21:16:26 +0900

    10/28 알고리즘

---
+++ gilyeon/Solution_lv2_땅따먹기.java
@@ -0,0 +1,19 @@
+﻿class Solution {
+    int solution(int[][] land) {
+//  dp 알고리즘 : 한 열씩 내려갈 때마다 나올 수 있는 최대값 계산하기
+        int answer = 0;
+        for(int i = 1; i < land.length; i++){
+            land[i][0] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][1] += Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][2] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][3]));
+            land[i][3] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][2]));
+        }
+
+        for(int i = 0; i < 4; i++){
+            if(land[land.length-1][i] > answer)
+                answer = land[land.length-1][i];
+        }
+
+        return answer;
+    }
+}
